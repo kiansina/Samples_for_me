1e6ed5a00357bc5c965f4a690877b422e7b77248
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-05-09 11:44:21 +0200

    Add files via upload

---
+++ del_pip.py
@@ -0,0 +1,3 @@
+pip install dash
+install waitress #to solve the issue of dashboarding https://exerror.com/warning-this-is-a-development-server-do-not-use-it-in-a-production-deployment-use-a-production-wsgi-server-instead/
+pip install chart-studio
