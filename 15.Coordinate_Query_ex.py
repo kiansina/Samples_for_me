8a34ae70a34d701e4f5abe0d57ee4e50f85df006
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-11-17 15:07:41 +0100

    Delete 15.Coordinate_Query_ex.py

--- 15.Coordinate_Query_ex.py
+++
@@ -1,7 +0,0 @@
-#find longitudes and latitudes
-from functools import partial
-from geopy.geocoders import Nominatim
-geolocator=Nominatim(user_agent="googleearth")
-geocode=partial(geolocator.geocode,language="en")
-location=geolocator.geocode(" POMPEI - Via Ripuaria 65")
-print(location.latitude,location.longitude)
